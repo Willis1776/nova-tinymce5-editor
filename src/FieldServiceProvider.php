f04eac81f011b7b37a402e4ff5eab7f6a34c09a0
Michael Willis <willis@clarity-hd.com>
2021-09-14 09:00:45 -0600

    Local storage of tinymce files

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -8,25 +8,32 @@ use Laravel\Nova\Nova;
 
 class FieldServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot()
-    {
-        Nova::serving(function (ServingNova $event) {
-            Nova::script('nova-tinymce5-editor', __DIR__.'/../dist/js/field.js');
-            Nova::style('nova-tinymce5-editor', __DIR__.'/../dist/css/field.css');
-        });
+  /**
+   * Bootstrap any application services.
+   */
+  public function boot()
+  {
+    Nova::serving(function (ServingNova $event) {
+      Nova::script('nova-tinymce5-editor', __DIR__ . '/../dist/js/field.js');
+      Nova::style('nova-tinymce5-editor', __DIR__ . '/../dist/css/field.css');
+    });
 
-        $this->publishes([
-            __DIR__.'/../config/nova-tinymce5-editor.php' => config_path('nova-tinymce5-editor.php'),
-        ], 'config');
-    }
+    $this->publishes(
+      [
+        realpath(__DIR__ . '/../dist/tinymce') => public_path('vendor/tinymce'),
+      ],
+      'public'
+    );
 
-    /**
-     * Register any application services.
-     */
-    public function register()
-    {
-    }
+    $this->publishes([
+      __DIR__ . '/../config/nova-tinymce5-editor.php' => config_path('nova-tinymce5-editor.php'),
+    ], 'config');
+  }
+
+  /**
+   * Register any application services.
+   */
+  public function register()
+  {
+  }
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,9 @@
 let mix = require('laravel-mix')
 
 mix
-  .setPublicPath('dist')
-  .js('resources/js/field.js', 'js')
-  .sass('resources/sass/field.scss', 'css')
+  .setPublicPath("dist")
+  .js("resources/js/field.js", "js")
+  .js("resources/js/tinymce.js", "js")
+  .sass("resources/sass/field.scss", "css");
+
+mix.copyDirectory("node_modules/tinymce/skins", "dist/tinymce/skins");
